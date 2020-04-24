197f2a813e95d93e72ddc360b9f4ae0625e5d896
ANDROID2468 <nicholas.steyer@gmail.com>
2020-04-24 01:47:28 -0500

    renamed potato to lineage

--- buildAll.sh
+++ buildAll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "ANDROID2468's build script for Potato OS"
+echo "ANDROID2468's build script for lineage OS"
 
 # removing old builds
 echo "deleting The out directory"
@@ -19,8 +19,8 @@ rm -f log_h810_usu.txt
 
 # vs986_usu
 echo "starting build for vs986_usu" | tee -a log_vs986_usu.txt
-breakfast potato_vs986_usu-userdebug | tee -a log_vs986_usu.txt
-brunch potato_vs986_usu-userdebug | tee -a log_vs986_usu.txt
+breakfast lineage_vs986_usu-userdebug | tee -a log_vs986_usu.txt
+brunch lineage_vs986_usu-userdebug | tee -a log_vs986_usu.txt
 echo "ARB (Anti-rollback) check " | tee -a log_vs986_usu.txt
 echo "md5 for venus.mbn" | tee -a log_vs986_usu.txt
 md5sum out/target/product/vs986_usu/system/etc/firmware/venus.mbn | tee -a log_vs986_usu.txt
@@ -28,8 +28,8 @@ echo "78e5cf520d0de4a413ef1cfa7bbbe713 should be the md5 (ARB of 2)" | tee -a lo
 
 # h810_usu
 echo "starting build for h810_usu" | tee -a log_h810_usu.txt
-breakfast potato_h810_usu-userdebug | tee -a log_h810_usu.txt
-brunch potato_h810_usu-userdebug | tee -a log_h810_usu.txt
+breakfast lineage_h810_usu-userdebug | tee -a log_h810_usu.txt
+brunch lineage_h810_usu-userdebug | tee -a log_h810_usu.txt
 echo "ARB (Anti-rollback) check " | tee -a log_h810_usu.txt
 echo "md5 for venus.mbn" | tee -a log_h810_usu.txt
 md5sum out/target/product/h810_usu/system/etc/firmware/venus.mbn | tee -a log_h810_usu.txt
@@ -37,8 +37,8 @@ echo "78e5cf520d0de4a413ef1cfa7bbbe713 should be the md5 (ARB of 2)" | tee -a lo
 
 # h811
 echo "starting build for h811" | tee -a log_h811.txt
-breakfast potato_h811-userdebug | tee -a log_h811.txt
-brunch potato_h811-userdebug | tee -a log_h811.txt
+breakfast lineage_h811-userdebug | tee -a log_h811.txt
+brunch lineage_h811-userdebug | tee -a log_h811.txt
 echo "ARB (Anti-rollback) check " | tee -a log_h811.txt
 echo "md5 for venus.mbn" | tee -a log_h811.txt
 md5sum out/target/product/h811/system/etc/firmware/venus.mbn | tee -a log_h811.txt
@@ -50,8 +50,8 @@ sleep 30m
 
 # h812_usu
 echo "starting build for h812" | tee -a log_h812_usu.txt
-breakfast potato_h812_usu-userdebug | tee -a log_h812_usu.txt
-brunch potato_h812_usu-userdebug | tee -a log_h812_usu.txt
+breakfast lineage_h812_usu-userdebug | tee -a log_h812_usu.txt
+brunch lineage_h812_usu-userdebug | tee -a log_h812_usu.txt
 echo "ARB (Anti-rollback) check " | tee -a log_h812_usu.txt
 echo "md5 for venus.mbn" | tee -a log_h812_usu.txt
 md5sum out/target/product/h812_usu/system/etc/firmware/venus.mbn | tee -a log_h812_usu.txt
@@ -59,8 +59,8 @@ echo "78e5cf520d0de4a413ef1cfa7bbbe713 should be the md5 (ARB of 2)" | tee -a lo
 
 # h815
 echo "starting build for h815" | tee -a log_h815.txt
-breakfast potato_h815-userdebug | tee -a log_h815.txt
-brunch potato_h815-userdebug | tee -a log_h815.txt
+breakfast lineage_h815-userdebug | tee -a log_h815.txt
+brunch lineage_h815-userdebug | tee -a log_h815.txt
 echo "ARB (Anti-rollback) check " | tee -a log_h815.txt
 echo "md5 for venus.mbn" | tee -a log_h815.txt
 md5sum out/target/product/h815/system/etc/firmware/venus.mbn | tee -a log_h815.txt
@@ -68,8 +68,8 @@ echo "d1f6fe863643b1e8d1e597762474928c should be the md5 (ARB 0f 0)" | tee -a lo
 
 # h815_usu
 echo "starting build for h815_usu" | tee -a log_h815_usu.txt
-breakfast potato_h815_usu-userdebug | tee -a log_h815_usu.txt
-brunch potato_h815_usu-userdebug | tee -a log_h815_usu.txt
+breakfast lineage_h815_usu-userdebug | tee -a log_h815_usu.txt
+brunch lineage_h815_usu-userdebug | tee -a log_h815_usu.txt
 echo "ARB (Anti-rollback) check " | tee -a log_h815_usu.txt
 echo "md5 for venus.mbn" | tee -a log_h815_usu.txt
 md5sum out/target/product/h815_usu/system/etc/firmware/venus.mbn | tee -a log_h815_usu.txt
